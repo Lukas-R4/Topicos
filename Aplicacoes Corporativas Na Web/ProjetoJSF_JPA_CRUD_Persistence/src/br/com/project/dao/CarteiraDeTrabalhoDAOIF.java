d8df2191856de46397463e1a9bd21801211f6d8a
Lukas-R4 <lucas93.rodrigues@gmail.com>
2020-04-21 21:30:38 -0300

    Deleted now
    asd

--- Aplicacoes Corporativas Na Web/ProjetoJSF_JPA_CRUD_Persistence/src/br/com/project/dao/CarteiraDeTrabalhoDAOIF.java
+++
@@ -1,7 +0,0 @@
-package br.com.project.dao;
-
-import br.com.project.model.CarteiraDeTrabalho;
-
-public interface CarteiraDeTrabalhoDAOIF extends GenericDAO<CarteiraDeTrabalho> {
-
-}
--- Aplicacoes Corporativas Na Web/ProjetoJSF_JPA_CRUD_Persistence/src/br/com/project/dao/FuncionarioDAOIF.java
+++
@@ -1,7 +0,0 @@
-package br.com.project.dao;
-
-import br.com.project.model.Funcionario;
-
-public interface FuncionarioDAOIF extends GenericDAO<Funcionario> {
-
-}
